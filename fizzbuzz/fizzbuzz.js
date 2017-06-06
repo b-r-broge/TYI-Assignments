9d8e1b80d7b6560f87a4f2520d6861ccafb970aa
Brad Broge <brbroge@gmail.com>
2017-06-06 14:19:25 -0400

    Adding fizzbuzz solution

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+for (var i = 1; i <= 100; i++) {
+  let out = "";
+  if ( i%3 === 0 ) {
+    out += "Fizz";
+  }
+  if ( i%5 === 0 ) {
+    out += "Buzz";
+  }
+  if ( out === "" ) {
+    out += i;
+  }
+  console.log(out);
+}
